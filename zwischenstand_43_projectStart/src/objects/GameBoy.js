5a41d8879a8264ced908b41bc65c21d812fd912f
YannicKuehn <yannickuehn@web.de>
2019-12-12 16:24:23 +0100

    screen implemented

--- zwischenstand_43_projectStart/src/objects/GameBoy.js
+++ zwischenstand_43_projectStart/src/objects/GameBoy.js
@@ -24,17 +24,17 @@ class GameBoy extends THREE.Group {
                 korpusMaterial.combine = THREE.MixOperation;
                 korpusMaterial.reflectivity = 0.005; */
 
-        let korpusGeometry = new THREE.BoxGeometry(10, 15, 3);
+        let korpusGeometry = new THREE.BoxGeometry(10, 15, 2.5);
         let korpus = new THREE.Mesh(korpusGeometry, korpusMaterial);
         korpus.castShadow = true;
         this.add(korpus);
 
         let batteryPackMaterial = korpusMaterial;
-        let batteryPackGeometry = new THREE.BoxGeometry(8, 10, 2);
+        let batteryPackGeometry = new THREE.BoxGeometry(8, 10, 1.5);
         let batteryPack = new THREE.Mesh(batteryPackGeometry, batteryPackMaterial);
         batteryPack.position.x = 0;
         batteryPack.position.y = 0;
-        batteryPack.position.z = -1.5;
+        batteryPack.position.z = -1.7;
         this.add(batteryPack);
 
         let abButtonMaterial = new THREE.MeshStandardMaterial({ color: 0xce42f5, roughness: 0.4, metalness: 0 });
@@ -45,14 +45,14 @@ class GameBoy extends THREE.Group {
 
         aButton.position.x = 3.5;
         aButton.position.y = -2.5;
-        aButton.position.z = 0.75;
+        aButton.position.z = 0.5;
 
         aButton.rotation.x = 90 * DEG_TO_RAD;
         this.add(aButton);
 
         bButton.position.x = 1.9;
         bButton.position.y = -3.45;
-        bButton.position.z = 0.75;
+        bButton.position.z = 0.5;
 
         bButton.rotation.x = 90 * DEG_TO_RAD;
         this.add(bButton);
@@ -65,12 +65,12 @@ class GameBoy extends THREE.Group {
 
         verticalCross.position.x = -2.5;
         verticalCross.position.y = -3;
-        verticalCross.position.z = 1.25;
+        verticalCross.position.z = 1;
         this.add(verticalCross);
 
         horizontalCross.position.x = -2.5;
         horizontalCross.position.y = -3;
-        horizontalCross.position.z = 1.25;
+        horizontalCross.position.z = 1;
         horizontalCross.rotation.z = 90 * DEG_TO_RAD;
         this.add(horizontalCross);
 
@@ -87,7 +87,7 @@ class GameBoy extends THREE.Group {
 
         startButton.position.x = 0.7;
         startButton.position.y = -5;
-        startButton.position.z = 1.4;
+        startButton.position.z = 1.2;
 
         startButton.rotation.z = 30 * DEG_TO_RAD;
 
@@ -98,12 +98,42 @@ class GameBoy extends THREE.Group {
 
         selectButton.position.x = -0.7;
         selectButton.position.y = -5;
-        selectButton.position.z = 1.4;
+        selectButton.position.z = 1.2;
 
         selectButton.rotation.z = 30 * DEG_TO_RAD;
 
         this.add(selectButton);
 
+
+        let screenCaseMaterial = new THREE.MeshStandardMaterial({ color: 0x4d4d4d, roughness: 0.4, metalness: 0 });
+        let screenCaseGeometry = new THREE.BoxGeometry(8,7,2);
+
+        let screenCase = new THREE.Mesh(screenCaseGeometry, screenCaseMaterial);
+
+        screenCase.position.y = 3;
+        screenCase.position.z = 0.27;
+
+        this.add(screenCase);
+
+        let screenMaterial = new THREE.MeshStandardMaterial({ color: 0x4b694a, roughness: 0.4, metalness: 0 });
+        let screenGeometry = new THREE.BoxGeometry(6, 5, 2);
+
+        let screen = new THREE.Mesh(screenGeometry, screenMaterial);
+
+        screen.position.y = 3;
+        screen.position.z = 0.3;
+
+        this.add(screen);
+
+        let controlLampMaterial = new THREE.MeshStandardMaterial({ color: 0x700329, roughness: 0.4, metalness: 0 });
+        let controlLampGeometry = new THREE.SphereGeometry(0.1, 16, 16);
+
+        let controlLamp = new THREE.Mesh(controlLampGeometry, controlLampMaterial);
+
+        controlLamp.position.x = -3.5;
+        controlLamp.position.y = 3;
+        controlLamp.position.z = 1.25;
+        this.add(controlLamp);
 
 
 
